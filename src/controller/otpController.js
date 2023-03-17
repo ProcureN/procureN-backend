f511e95e0b6211f1b3cbc732082098d3442349f6
veronica17-web <veronicamiryala001@gmail.com>
2023-03-17 16:10:08 +0530

    sending the role too

--- src/controller/otpController.js
+++ src/controller/otpController.js
@@ -1,27 +1,39 @@
 const optModel = require("../models/OtpModel")
+const costumerModel = require("../models/CostumerModel")
 const validator = require("../validation/validations")
 const mongoose = require("mongoose")
 
 const otpVerification = async (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    let data = req.body
-    let { Email, otp } = data
-    if (!Email) return res.status(400).send({ status: false, message: "User Email-id is required" });
-        //validating user email-id
-        if (!validator.isValidEmail(Email.trim())) return res.status(400).send({ status: false, message: "Please Enter a valid Email-id" });
-        if (!otp) return res.status(400).send({ status: false, message: "otp is required" });
-        
-    let otpData = await optModel.findOne({ Email: Email })
-    if (!otpData) {
-        return res.send("incorrect Email")
+    try {
+        res.setHeader('Access-Control-Allow-Origin', '*')
+        let data = req.body
+        let { Email, otp } = data
+        if (!Email) return res.status(400).send({ status: false, message: "User Email-id is required" });
+            //validating user email-id
+            if (!validator.isValidEmail(Email.trim())) return res.status(400).send({ status: false, message: "Please Enter a valid Email-id" });
+            let costumerData = await costumerModel.findOne({Email:Email})
+            if(!costumerData){
+                return res.status(400).send({status:false,Message:"user not found"})
+            }
+
+            if (!otp) return res.status(400).send({ status: false, message: "otp is required" });
+            
+        let otpData = await optModel.findOne({ Email: Email })
+        if (!otpData) {
+            return res.status(400).send("incorrect Email")
+        }
+        let otpverify = otpData.toObject()
+       costumerData.toObject()
+        console.log(otpverify.otp,costumerData.SelectRole)
+        if (otp == otpverify.otp) {
+            return res.status(200).send({status:true,message:"login successful",SelectRole:costumerData.SelectRole.toString()})
+        } else {
+            return res.status(400).send({status:false,message:"incorrect otp"})
+        }
+    } catch (error) {
+        return res.status(500).send({ status: false, message: error.message })
     }
-    let otpverify = otpData.toObject()
-    console.log(otpverify.otp)
-    if (otp == otpverify.otp) {
-        return res.send("login successful")
-    } else {
-        return res.send("incorrect otp")
-    }
+   
 }
 
 module.exports = { otpVerification }
