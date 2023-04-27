f87a763c344b1c68ca1c1e26ce5d041b9eac6b25
veronica17-web <veronicamiryala001@gmail.com>
2023-04-27 17:32:41 +0530

    removed validation for contact form

--- src/controller/contactUsController.js
+++ src/controller/contactUsController.js
@@ -5,22 +5,22 @@ const contactform =async (req,res)=>{
     let data = req.body
     let {name,email,subject,message,phone}= data
     
-    if (validator.isValidBody(data)) return res.status(400).send({ status: false, message: "Enter details to create your account" });
+    // if (validator.isValidBody(data)) return res.status(400).send({ status: false, message: "Enter details to create your account" });
  
-    if (!name) return res.status(400).send({ status: false, message: "name is required" });
-    if (validator.isValid(name)) return res.status(400).send({ status: false, message: "name should not be an empty string" });
+    // if (!name) return res.status(400).send({ status: false, message: "name is required" });
+    // if (validator.isValid(name)) return res.status(400).send({ status: false, message: "name should not be an empty string" });
 
-    if (!email) return res.status(400).send({ status: false, message: "email is required" });
-    if (!validator.isValidEmail(email.trim())) return res.status(400).send({ status: false, message: "Please Enter a valid Email-id" });
-if(subject){
-    if (validator.isValid(subject)) return res.status(400).send({ status: false, message: "subject should not be an empty string" });
-}
-if(message){
-    if (validator.isValid(message)) return res.status(400).send({ status: false, message: "message should not be an empty string" });
-}
-if(phone){
-    if (!validator.isValidPhone(phone.trim())) return res.status(400).send({ status: false, message: "Please Enter a valid Phone number" });
-}
+//     if (!email) return res.status(400).send({ status: false, message: "email is required" });
+//     if (!validator.isValidEmail(email.trim())) return res.status(400).send({ status: false, message: "Please Enter a valid Email-id" });
+// if(subject){
+//     if (validator.isValid(subject)) return res.status(400).send({ status: false, message: "subject should not be an empty string" });
+// }
+// if(message){
+//     if (validator.isValid(message)) return res.status(400).send({ status: false, message: "message should not be an empty string" });
+// }
+// if(phone){
+//     if (!validator.isValidPhone(phone.trim())) return res.status(400).send({ status: false, message: "Please Enter a valid Phone number" });
+// }
 var currentdate = new Date();
 var datetime = currentdate.getDate() + "-" + (currentdate.getMonth()+1)
     + "-" + currentdate.getFullYear()
