4ad974dd3baf73834ae2a2cc3f1f3f8706f08818
ProcureN <132357850+ProcureN@users.noreply.github.com>
2023-05-22 11:05:48 +0530

    backend procureN
    uploading files for the 1st time

--- src/route/route.js
+++ src/route/route.js
@@ -12,6 +12,8 @@ const {addProdcts, updateProduct, DeleteProduct, getProducts, getManufactureProd
 
 router.get("/test-me", function (req, res) {
   res.send("this is successfully created");
+
+  
 });
 //=============================costumer==================================
 
