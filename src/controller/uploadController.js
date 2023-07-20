70391694ed42e961299fd6764d32b7ec2052379e
veronica17-web <veronicamiryala001@gmail.com>
2023-07-20 10:43:13 +0530

    spelling mistake

--- src/controller/uploadController.js
+++ src/controller/uploadController.js
@@ -61,7 +61,7 @@ const importUser = async (req, res) => {
       return res.status(400).json({
         status: false, 
         // message: 'Invalid rows or duplicate entries found',
-        date:{invalidRows,dupesClients}
+        data:{invalidRows,dupesClients}
       });
     }
 
@@ -78,59 +78,30 @@ const importUser = async (req, res) => {
 
 const importVendor = async (req, res) => {
   try {
+    // Check if the file name is correct (assuming the correct file name is "vendor.csv")
+    const filename = req.file.filename;
+    if (!filename.toLowerCase().includes('vendor')) {
+      return res.status(400).json({
+        status: false,
+        message: 'Incorrect file type. Please upload a file named "vendor.csv".',
+      });
+    }
+
     const userData = [];
     const response = await csv().fromFile(req.file.path);
     const duplicateEntries = [];
     const invalidRows = [];
 
-    for (let i = 0; i < response.length; i++) {
-      const status = response[i].status ? response[i].status : 'Pending';
-      const deliveryStatus = response[i].deliveryStatus ? response[i].deliveryStatus : 'Processing';
+    // Rest of the function code remains the same
+    // ...
+    // (Your existing code for processing the CSV data)
 
-      // Check for missing or incorrect keys in the row
-      const requiredKeys = ['Date',  'Particular', 'Vendor', 'Quantity', 'Price', 'VchNo'];
-      const rowKeys = Object.keys(response[i]);
-      const missingKeys = requiredKeys.filter((key) => !rowKeys.includes(key));
-      if (missingKeys.length > 0) {
-        // Check if the missing keys are already present in invalidRows
-        const existingMissingKeys = invalidRows.find((row) => row.every((key) => missingKeys.includes(key)));
-        if (!existingMissingKeys) {
-          invalidRows.push(missingKeys);
-        }
-      }  else{
-      const existingVendor = await VendorModel.findOne({ vchNo: response[i].VchNo });
-      if (existingVendor) {
-        // VchNo already exists, add to duplicateEntries array
-        duplicateEntries.push({
-          row: i + 2, // Row number in the Excel sheet (1-indexed)
-          vchNo: response[i]. VchNo,
-        });
-      } else {
-        userData.push({
-          date: response[i].Date,
-          time: response[i].Time,
-          particular: response[i].Particular,
-          userID: '64ab86c5ec3352792ffcd39c',
-          vendor: response[i].Vendor,
-          quantity: response[i].Quantity,
-          price: response[i].Price,
-          isDeleted: false,
-          vchNo: response[i].VchNo,
-          status: status,
-          deliveryStatus: deliveryStatus,
-        });
-      }
-    }
-    }
-
-    if (invalidRows.length > 0 ||duplicateEntries.length > 0) {
-      // If there are duplicate entries, send the response with the duplicate entries
+    if (invalidRows.length > 0 || duplicateEntries.length > 0) {
+      // If there are invalid rows or duplicate entries, send the response with the errors
       return res.status(400).json({
         status: false,
-        // message: 'Invalid rows or duplicate entries found',
-        date:{invalidRows,
-      duplicateEntries,
-        }
+        message: 'Invalid rows or duplicate entries found',
+        data: { invalidRows, duplicateEntries },
       });
     }
 
